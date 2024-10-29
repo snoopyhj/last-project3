d15c7e451ce164a4f80ee65dd281b7d6a368f107
Hyeonji Jo <hyunji5556@naver.com>
2024-10-29 18:02:03 +0900

    챗봇

--- chatbottest5/src/main/java/com/example/demo/ChatbotApplication.java
+++ chatbottest5/src/main/java/com/example/demo/ChatbotApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChatbottestApplication {
+public class ChatbotApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ChatbottestApplication.class, args);
+		SpringApplication.run(ChatbotApplication.class, args);
 	}
 
 }
